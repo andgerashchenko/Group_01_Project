a3e6fe8ca77a6cc4cc7ec9b2d02179285c427d54
Andrew Gerashchenko <andgerashchenko@gmail.com>
2022-06-22 20:43:21 -0400

    commit

---
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -0,0 +1,47 @@
+package com.cydeo.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+    // Creating private constructor, to hide object
+    private Driver() {
+    }
+
+    // make variable private ti hide it
+    // make it static, so we can use it in static method
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            String browserType = ConfigReader.getProperty("browser");
+            switch (browserType) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+            }
+        }
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver=null;
+
+        }
+    }
+}
